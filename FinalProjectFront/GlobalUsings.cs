4402470424484420f6d25199485cd2bbaa80ab5e
Andu <gintokiyoo@gmail.com>
2023-03-23 21:24:02 +0200

    Delete FinalProjectFront directory

--- FinalProjectFront/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-﻿global using Infrastructure.DataLogic;
-global using Infrastructure.DataLogic.Interface;
-global using Infrastructure.DbAccess;
-global using Infrastructure.DbAccess.Interface;
-global using Content.Models;
