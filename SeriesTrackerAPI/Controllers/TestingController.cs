c29355b5cf4f1a57aaa90cc70bd0b13ffb683838
Alex <gintokiyoo@gmail.com>
2023-03-23 21:21:48 +0200

    Added start of API with simple data

---
+++ SeriesTrackerAPI/Controllers/TestingController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeriesTrackerAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("/SeriesTrackerAPIRoute")]
+    public class TestingController : ControllerBase
+    {
+        private PersonModel _person = new PersonModel 
+        {
+            PersonAge = 5,
+            PersonCountryOfOrigin = "Ro",
+            PersonId = 12,
+            PersonName = "Foo",
+        };
+
+        [HttpGet]
+        public PersonModel SeriesTrackerAPIGet()
+        {
+            return _person;
+        }
+
+        [HttpPost]
+        public void SeriesTrackerAPIPost(PersonModel person)
+        {
+            _person = person;
+        }
+
+        [HttpPut]
+        public void SeriesTrackerAPIPut(PersonModel person)
+        {
+            _person = person;
+        }
+    }
+}
